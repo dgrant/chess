027ed8c30e9a2816d298bbb6bf449696e114e233
David Grant <david.grant@poweredbygrow.com>
2018-09-04 01:48:21 -0700

    Initial positions and printing to screen done

--- chesslib/src/board.rs
+++ chesslib/src/board.rs
@@ -1,10 +1,27 @@
-static PAWN: &'static str = "p";
-static ROOK: &'static str = "R";
-static KNIGHT: &'static str = "N";
-static BISHOP: &'static str = "B";
-static QUEEN: &'static str = "Q";
-static KING: &'static str = "K";
-static SPACE: &'static str = " ";
+pub static W_PAWN: &'static str = "♙";
+pub static W_ROOK: &'static str = "♖";
+pub static W_KNIGHT: &'static str = "♘";
+pub static W_BISHOP: &'static str = "♗";
+pub static W_QUEEN: &'static str = "♕";
+pub static W_KING: &'static str = "♔";
+pub static W_SPACE: &'static str = " ";
+
+pub static B_PAWN: &'static str = "♟";
+pub static B_ROOK: &'static str = "♜";
+pub static B_KNIGHT: &'static str = "♞";
+pub static B_BISHOP: &'static str = "♝";
+pub static B_QUEEN: &'static str = "♛";
+pub static B_KING: &'static str = "♚";
+pub static B_SPACE: &'static str = " ";
+
+pub static A: &'static str = "a";
+pub static B: &'static str = "b";
+pub static C: &'static str = "c";
+pub static D: &'static str = "d";
+pub static E: &'static str = "e";
+pub static F: &'static str = "f";
+pub static G: &'static str = "g";
+pub static H: &'static str = "h";
 
 
 pub struct Board {
@@ -30,12 +47,12 @@ pub fn get_starting_board() -> Board {
         w_rooks: (1 << (0 + 0)) + (1 << (0 + 7)),
         w_queen: (1 << (0 + 3)),
         w_king: (1 << (0 + 4)),
-        b_pawns: 0,
-        b_knights: 0,
-        b_bishops: 0,
-        b_rooks: 0,
-        b_queen: 0,
-        b_king: 0,
+        b_pawns: (1 << (6*8 + 0)) + (1 << (6*8 + 1)) + (1 << (6*8 + 2)) + (1 << (6*8 + 3)) + (1 << (6*8 + 4)) + (1 << (6*8 + 5)) + (1 << (6*8 + 6)) + (1 << (6*8 + 7)),
+        b_knights: (1 << (7*8 + 1)) + (1 << (7*8 + 6)),
+        b_bishops: (1 << (7*8 + 2)) + (1 << (7*8 + 5)),
+        b_rooks: (1 << (7*8 + 0)) + (1 << (7*8 + 7)),
+        b_queen: (1 << (7*8 + 3)),
+        b_king: (1 << (7*8 + 4)),
     };
     board
 }
@@ -55,6 +72,21 @@ pub fn string_file_to_int(file: &str) -> u8 {
     }
 }
 
+pub fn int_file_to_string(file: u8) -> &'static str {
+    match file {
+        0 => A,
+        1 => B,
+        2 => C,
+        3 => D,
+        4 => E,
+        5 => F,
+        6 => G,
+        7 => H,
+//        TODO(dgrant): Handle this differently
+        _ => W_SPACE
+    }
+}
+
 pub fn convert_coordinate_to_bitboard_index(coordinate: &str) -> u8 {
     let first_char = &coordinate[0..1];
     let file_number = string_file_to_int(first_char);
@@ -69,22 +101,42 @@ pub fn is_bit_set(bitboard: u64, bit: u8) -> bool {
 pub fn get_piece_at_coordinate(board: &Board, coordinate: &str) -> &'static str {
     let bitboard_index = convert_coordinate_to_bitboard_index(coordinate);
     if is_bit_set(board.w_pawns, bitboard_index) {
-        return PAWN;
+        return W_PAWN;
     } else if is_bit_set(board.w_knights, bitboard_index) {
-        return KNIGHT;
+        return W_KNIGHT;
     } else if is_bit_set(board.w_bishops, bitboard_index) {
-        return BISHOP;
+        return W_BISHOP;
     } else if is_bit_set(board.w_queen, bitboard_index) {
-        return QUEEN;
+        return W_QUEEN;
     } else if is_bit_set(board.w_rooks, bitboard_index) {
-        return ROOK;
+        return W_ROOK;
     } else if is_bit_set(board.w_king, bitboard_index) {
-        return KING;
+        return W_KING;
+    } else if is_bit_set(board.b_pawns, bitboard_index) {
+        return B_PAWN;
+    } else if is_bit_set(board.b_knights, bitboard_index) {
+        return B_KNIGHT;
+    } else if is_bit_set(board.b_bishops, bitboard_index) {
+        return B_BISHOP;
+    } else if is_bit_set(board.b_queen, bitboard_index) {
+        return B_QUEEN;
+    } else if is_bit_set(board.b_king, bitboard_index) {
+        return B_KING;
+    } else if is_bit_set(board.b_rooks, bitboard_index) {
+        return B_ROOK;
     } else {
-        return SPACE;
+        return W_SPACE;
     }
 }
 
-pub fn print_board(board: &Board) {
-    
+pub fn print_board(_board: &Board) {
+    for rank in (0..8).rev() {
+        for file in 0..8 {
+            let coordinate = &format!("{}{}", int_file_to_string(file), (rank + 1).to_string());
+//            print!("coordinate:{}\n", coordinate);
+            let piece = get_piece_at_coordinate(_board, coordinate);
+            print!("{}", piece);
+        }
+        print!("\n");
+    }
 }
--- chesslib/src/lib.rs
+++ chesslib/src/lib.rs
@@ -12,25 +12,27 @@ mod tests {
     #[test]
     fn test() {
         let board = board::get_starting_board();
-        assert_eq!(board::get_piece_at_coordinate(&board, "b2"), "p");
-        assert_eq!(board::get_piece_at_coordinate(&board, "c2"), "p");
-        assert_eq!(board::get_piece_at_coordinate(&board, "d2"), "p");
-        assert_eq!(board::get_piece_at_coordinate(&board, "e2"), "p");
-        assert_eq!(board::get_piece_at_coordinate(&board, "a2"), "p");
-        assert_eq!(board::get_piece_at_coordinate(&board, "f2"), "p");
-        assert_eq!(board::get_piece_at_coordinate(&board, "g2"), "p");
-        assert_eq!(board::get_piece_at_coordinate(&board, "h2"), "p");
+        assert_eq!(board::get_piece_at_coordinate(&board, "b2"), board::W_PAWN);
+        assert_eq!(board::get_piece_at_coordinate(&board, "c2"), board::W_PAWN);
+        assert_eq!(board::get_piece_at_coordinate(&board, "d2"), board::W_PAWN);
+        assert_eq!(board::get_piece_at_coordinate(&board, "e2"), board::W_PAWN);
+        assert_eq!(board::get_piece_at_coordinate(&board, "a2"), board::W_PAWN);
+        assert_eq!(board::get_piece_at_coordinate(&board, "f2"), board::W_PAWN);
+        assert_eq!(board::get_piece_at_coordinate(&board, "g2"), board::W_PAWN);
+        assert_eq!(board::get_piece_at_coordinate(&board, "h2"), board::W_PAWN);
 
-        assert_eq!(board::get_piece_at_coordinate(&board, "a1"), "R");
-        assert_eq!(board::get_piece_at_coordinate(&board, "h1"), "R");
+        assert_eq!(board::get_piece_at_coordinate(&board, "a1"), board::W_ROOK);
+        assert_eq!(board::get_piece_at_coordinate(&board, "h1"), board::W_ROOK);
 
-        assert_eq!(board::get_piece_at_coordinate(&board, "b1"), "N");
-        assert_eq!(board::get_piece_at_coordinate(&board, "g1"), "N");
+        assert_eq!(board::get_piece_at_coordinate(&board, "b1"), board::W_KNIGHT);
+        assert_eq!(board::get_piece_at_coordinate(&board, "g1"), board::W_KNIGHT);
 
-        assert_eq!(board::get_piece_at_coordinate(&board, "c1"), "B");
-        assert_eq!(board::get_piece_at_coordinate(&board, "f1"), "B");
+        assert_eq!(board::get_piece_at_coordinate(&board, "c1"), board::W_BISHOP);
+        assert_eq!(board::get_piece_at_coordinate(&board, "f1"), board::W_BISHOP);
 
-        assert_eq!(board::get_piece_at_coordinate(&board, "d1"), "Q");
-        assert_eq!(board::get_piece_at_coordinate(&board, "e1"), "K");
+        assert_eq!(board::get_piece_at_coordinate(&board, "d1"), board::W_QUEEN);
+        assert_eq!(board::get_piece_at_coordinate(&board, "e1"), board::W_KING);
+
+        board::print_board(&board);
     }
 }
